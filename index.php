4cfa4ec6a5d1b52c83fe0e1d928842325e3a1126
wirtenhase <145996701+wirtenhase@users.noreply.github.com>
2026-06-16 21:13:21 +0200

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php require_once __DIR__ . '/includes/config.php'; ?>
-<!DOCTYPE html>
-<html lang="de">
-<?php require __DIR__ . '/includes/head.php'; ?>
-<?php require __DIR__ . '/includes/layout.php'; ?>
-<?php require __DIR__ . '/includes/scripts.php'; ?>
-</html>
