21ee23fc05ac936512feb23f9245e2e666f78ba0
bohlkaukai-alt <bohlkaukai@gmail.com>
2026-06-19 08:46:14 +0200

    Update social media links in social.js

--- assets/js/social.js
+++ assets/js/social.js
@@ -17,14 +17,20 @@ function applySocialLinks() {
         a.target = "_blank";
         a.rel = "noopener";
     });
+    document.querySelectorAll('[data-social="facebook"], #impressum-facebook').forEach(a => {
+        a.href = links.facebook || "https://www.facebook.com/";
+        a.target = "_blank";
+        a.rel = "noopener";
+    });
 }
 
 function socialFooterMarkup() {
     return `<div class="social-footer">
         <a href="impressum.html">Impressum</a>
         <a href="datenschutz.html">Datenschutz</a>
-        <a data-social="instagram" href="https://www.instagram.com/" target="_blank" rel="noopener">Instagram</a>
-        <a data-social="tiktok" href="https://www.tiktok.com/" target="_blank" rel="noopener">TikTok</a>
+        <a data-social="instagram" href="https://www.instagram.com/minijobfinder?igsh=dnNzYnZ6MndpODN4" target="_blank" rel="noopener">Instagram</a>
+        <a data-social="tiktok" href="https://pro.tiktok.com/t/ZG9js9GAC9k4A-fhmKc/" target="_blank" rel="noopener">TikTok</a>
+        <a data-social="facebook" href="https://www.facebook.com/share/17g7jF28Tt/" target="_blank" rel="noopener">Facebook</a>
     </div>`;
 }
 
