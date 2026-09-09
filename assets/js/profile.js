b716044bf24d0b1ff9729c6bdbe7d35b4dbf3d06
Kai Böhlkau <bohlkaukai@gmail.com>
2026-09-09 18:17:28 +0200

    Update: Gastzugang, EXE Download v1.1.0, SW Cache

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -131,7 +131,7 @@ function openDownloadModal() {
                     </div>
                     <span class="material-icons download-arrow">download</span>
                 </a>
-                <a href="${releaseBase}/MiniJob.Finder.Setup.1.0.0.exe" class="download-option" download>
+                <a href="${releaseBase}/MiniJob%20Finder%20Setup%201.1.0.exe" class="download-option" download>
                     <div class="download-icon">🖥️</div>
                     <div class="download-info">
                         <strong>Windows</strong>
